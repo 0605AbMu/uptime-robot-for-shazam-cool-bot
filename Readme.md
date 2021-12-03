0c0700d8d664e88ac23c83ddbb528b762840359c
0605AbMu <71525239+0605AbMu@users.noreply.github.com>
2021-12-04 02:36:48 +0500

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Glitch serveri uchun uptime robot
---
+++ index.js
@@ -0,0 +1,13 @@
+
+const axios  = require("axios").default
+const fs = require("fs");
+setInterval(() => {
+    axios("https://seasoned-abrupt-zucchini.glitch.me", {method:"get"})
+.then(res=>{
+    fs.appendFileSync("./data/log.txt",res.data+" Date: "+new Date()+"\n" )
+})
+.catch(e=>{
+    fs.appendFileSync("./data/log.txt",("Error has Occured "+" Date: "+new Date())+"\n")
+})
+}, 330000 );
+
